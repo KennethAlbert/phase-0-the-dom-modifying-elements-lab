4ae54a7abe7bf1804bae5736fc6ba29be6c585e8
Kenneth Albert <gichuka@gmail.com>
2022-06-03 15:04:12 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+const main=document.querySelector("#main");
+
+main.parentElement.removeChild(main);
+
+const newHeader= document.createElement("h1");
+newHeader.setAttribute('id','victory');
+newHeader.innerHTML="Kenneth is the champion";
+
+document.body.appendChild(newHeader);
+
+
+
+
+
+
